ba61e5a60b6a20eb421a3b86884ee650c008875e
voltodev <94715629+voltodev@users.noreply.github.com>
2021-11-23 14:24:09 -0300

    update msg

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Nova mensagem no devops"
 
 if __name__ == '__main__':
     app.run()
