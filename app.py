a005aef9f0617552124ce07bfc2076510a186da2
Jefferson Clark <jeffersonclarkti@gmail.com>
2021-11-24 10:38:14 -0300

    Novo teste de bug no sonar

--- app.py
+++ app.py
@@ -7,7 +7,16 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Novo teste para o gustavo"
+    return "Novo teste de bug"
+
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
 
 if __name__ == '__main__':
     app.run()
