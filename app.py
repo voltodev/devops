8b3012f9b92959602fc9691fc1ce0591cc8ab2ad
Jefferson Clark <jeffersonclarkti@gmail.com>
2021-11-24 10:30:55 -0300

    teste gustavo com erro

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Utilizando removendo mensagem do git flow"
+    return "Novo teste para o gustavo"
 
 if __name__ == '__main__':
     app.run()
